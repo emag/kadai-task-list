a9cc1e324d0dfe11df69c3b092b6349cf980b896
emag <lanabe.lanabe@gmail.com>
2017-06-20 03:52:07 +0900

    Create GET /tasks/:id/edit

--- app/controllers/UpdateTaskController.scala
+++ app/controllers/UpdateTaskController.scala
@@ -2,6 +2,8 @@ package controllers
 
 import javax.inject.{ Inject, Singleton }
 
+import forms.TaskForm
+import models.Task
 import play.api.i18n.{ I18nSupport, MessagesApi }
 import play.api.mvc.{ Action, AnyContent, Controller }
 
@@ -11,7 +13,11 @@ class UpdateTaskController @Inject()(val messagesApi: MessagesApi)
     with I18nSupport
     with TaskControllerSupport {
 
-  def index(taskId: Long): Action[AnyContent] = ???
+  def index(taskId: Long): Action[AnyContent] = Action { implicit request =>
+    val result     = Task.findById(taskId).get
+    val filledForm = form.fill(TaskForm(result.id, result.content))
+    Ok(views.html.edit(filledForm))
+  }
 
   def update: Action[AnyContent] = ???
 
