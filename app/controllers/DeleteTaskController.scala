b332340961d481a2ffd6bd107859a7a76ff288e7
emag <lanabe.lanabe@gmail.com>
2017-06-20 04:03:16 +0900

    Create POST /tasks/:id/delete

--- app/controllers/DeleteTaskController.scala
+++ app/controllers/DeleteTaskController.scala
@@ -2,12 +2,22 @@ package controllers
 
 import javax.inject.{ Inject, Singleton }
 
-import play.api.i18n.{ I18nSupport, MessagesApi }
+import models.Task
+import play.api.i18n.{ I18nSupport, Messages, MessagesApi }
 import play.api.mvc.{ Action, AnyContent, Controller }
+import scalikejdbc.AutoSession
 
 @Singleton
 class DeleteTaskController @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport {
 
-  def delete(taskId: Long): Action[AnyContent] = ???
+  def delete(taskId: Long): Action[AnyContent] = Action {
+    implicit val session = AutoSession
+    val result           = Task.deleteById(taskId)
+    if (result > 0) {
+      Redirect(routes.GetTasksController.index())
+    } else {
+      InternalServerError(Messages("DeleteTaskError"))
+    }
+  }
 
 }
